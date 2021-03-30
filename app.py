d86ed63e5dc4a04e7448b25f3c65756ba048cdff
Divya-1200 <divya07msd@example.com>
2021-03-30 14:13:15 +0530

    base code

---
+++ app.py
@@ -0,0 +1,29 @@
+'''
+Source:
+
+Author: Raja CSP
+
+
+'''
+from flask import Flask,render_template
+import random
+import json
+
+app  = Flask(__name__)
+PORT = 3024
+
+    
+@app.route("/", methods=["GET","POST"])
+def startpy():
+
+    result = {
+
+        "Greetings" : "Tactlabs welcomes you"
+    }
+
+    return render_template("index.html", result = result)
+
+
+if __name__ == "__main__":
+    app.run( debug = True,host="0.0.0.0",port = PORT)
+    
