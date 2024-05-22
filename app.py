122881ceb98ad076a7ee8fb500f6893ae324c3c9
Raja CSP Raman <raja.raman@syndigo.com>
2024-05-22 14:29:17 +0530

    vercel conf added

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ import json
 app  = Flask(__name__)
 PORT = 3011
 
-    
+
 @app.route("/", methods=["GET","POST"])
 def startpy():
 
@@ -21,11 +21,10 @@ def startpy():
         "random_number" : random.randint(20, 100)
     }
 
-    return result
+    # return result
 
-    # return render_template("index.html", result = result)
+    return render_template("index.html", result = result)
 
 
 if __name__ == "__main__":
     app.run( debug = True,host="0.0.0.0",port = PORT)
-    
